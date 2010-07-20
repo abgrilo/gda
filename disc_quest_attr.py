69912cd6b1be03a9ab72a480774b004a5e4c6c19
Vitor Massaru Iha <v.massaru@gmail.com>
2010-07-20 11:47:37 -0300

    arrumado bug: testa se a disciplina já foi adicionada antes, troquei tabs por espaços

--- disc_quest_attr.py
+++ disc_quest_attr.py
@@ -7,124 +7,127 @@ Licença: GPLv3 ou mais recente
 """
 
 teoricas = [
-	'MC348', 
-	'MC448', 
-	'MC522', 
-	'MC526', 
-	'MC668', 
-	'MC704', 
-	'MC722', 
-	'MC822', 
-	'MC823', 
-	'MC878', 
-	'MC898', 
-	'MC910', 
-	'MC938', 
-	'MC998', 
-	'MO405', 
-	'MO417', 
-	'MO422', 
-	'MO441', 
-	'MO637', 
+  'MC348', 
+  'MC448', 
+  'MC522', 
+  'MC526', 
+  'MC668', 
+  'MC704', 
+  'MC722', 
+  'MC822', 
+  'MC823', 
+  'MC878', 
+  'MC898', 
+  'MC910', 
+  'MC938', 
+  'MC998', 
+  'MO405', 
+  'MO417', 
+  'MO422', 
+  'MO441', 
+  'MO637', 
 ]
 
 praticas = [ 
-	'MC427', 
-	'MC715', 
+  'MC427', 
+  'MC715', 
 ]
 
 teoricas_praticas = [
-	'MC102', 
-	'MC202', 
-	'MC326', 
-	'MC336', 
-	'MC404', 
-	'MC436', 
-	'MC514', 
-	'MC536', 
-	'MC542', 
-	'MC548', 
-	'MC636', 
-	'MC750', 
-	'MC906', 
-	'MC930', 
-	'MO640', 
+  'MC102', 
+  'MC202', 
+  'MC326', 
+  'MC336', 
+  'MC404', 
+  'MC436', 
+  'MC514', 
+  'MC536', 
+  'MC542', 
+  'MC548', 
+  'MC636', 
+  'MC750', 
+  'MC906', 
+  'MC930', 
+  'MO640', 
 ]
 
 estagio = [
-	'MC019', 
-	'MC030', 
-	'MC032', 
-	'MC040', 
-	'MC041', 
-	'MC050', 
-	'MO669', 
-	'MO800', 
+  'MC019', 
+  'MC030', 
+  'MC032', 
+  'MC040', 
+  'MC041', 
+  'MC050', 
+  'MO669', 
+  'MO800', 
 ]
 
 topicos = [
-	'MO805', 
-	'MO806', 
-	'MO809', 
-	'MO815', 
-	'MO818', 
-	'MO825', 
-	'MO827', 
-	'MO829', 
-	'MC914', 
-	'MC918', 
-	'MC919', 
-	'MC953', 
-	'MC964', 
-	'MC976', 
+  'MO805', 
+  'MO806', 
+  'MO809', 
+  'MO815', 
+  'MO818', 
+  'MO825', 
+  'MO827', 
+  'MO829', 
+  'MC914', 
+  'MC918', 
+  'MC919', 
+  'MC953', 
+  'MC964', 
+  'MC976', 
 ]
 
 teoricas.extend([
 #No lo se - Grad
-	'MC039', 
-	'MC852', 
-	'MC923', 
-	'MC940', 
-	'MC950', 
-	'MC962', 
+  'MC039', 
+  'MC852', 
+  'MC923', 
+  'MC940', 
+  'MC950', 
+  'MC962', 
 #No lo se - PosGrad
-	'MO409', 
-	'MO410', 
-	'MO416', 
-	'MO445', 
-	'MO601', 
-	'MO603', 
-	'MO645', 
-	'MO648', 
-	'MO649', 
-	'MO650', 
-	'MO901', 
+  'MO409', 
+  'MO410', 
+  'MO416', 
+  'MO445', 
+  'MO601', 
+  'MO603', 
+  'MO645', 
+  'MO648', 
+  'MO649', 
+  'MO650', 
+  'MO901', 
 ])
 
 def main():
-	from sad.models import Disciplina, Questionario
+  from sad.models import Disciplina, Questionario
 
-	# montando um dicionário das matérias com o tipo de questionário delas
-	discs = {}
-	for tipo in ('teoricas', 'praticas', 'teoricas_praticas', 'estagio', 'topicos'):
-		for sigla in eval(tipo): # pega a lista com o nome da string
-			discs[sigla] = tipo  # e.g discs['MC102'] = 'teorico_praticas'
-	
-	# adiciona o questionário correto para todas as matérias
-	for sigla in discs:
-		print 'Incluindo a disciplina %s no questionario %s' % (sigla, discs[sigla])
-		d = Disciplina.objects.filter(sigla=sigla)[0]
-		try:
-			q = Questionario.objects.filter(tipo=discs[sigla])[0]
-		except:
-			# no existe esto questionario!!!
-			q = Questionario(tipo=discs[sigla], texto=discs[sigla], semestre='2008-08-01')
-			q.save()
-		d.questionario = q
-		d.save()
+  # montando um dicionário das matérias com o tipo de questionário delas
+  discs = {}
+  for tipo in ('teoricas', 'praticas', 'teoricas_praticas', 'estagio', 'topicos'):
+    for sigla in eval(tipo): # pega a lista com o nome da string
+      discs[sigla] = tipo  # e.g discs['MC102'] = 'teorico_praticas'
+  
+  # adiciona o questionário correto para todas as matérias
+  for sigla in discs:
+    print 'Incluindo a disciplina %s no questionario %s' % (sigla, discs[sigla])
+    try:
+      d = Disciplina.objects.filter(sigla=sigla)[0]
+    except:
+      d = Disciplina(sigla=sigla)
+    try:
+      q = Questionario.objects.filter(tipo=discs[sigla])[0]
+    except:
+      # no existe esto questionario!!!
+      q = Questionario(tipo=discs[sigla], texto=discs[sigla], semestre='2008-08-01')
+      q.save()
+    d.questionario = q
+    d.save()
 
-	# done!
+  # done!
 
 if __name__ == "__main__":
-	main()
+  main()
 
