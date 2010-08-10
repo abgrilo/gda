a98ad8cce121dd169ebfb93d0421214c2361b073
Vitor Massaru Iha <v.massaru@gmail.com>
2010-08-10 13:27:19 -0300

    com a retirada do link simbólico, foi arrumada a configuração em settings

--- settings.py
+++ settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'caco.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -82,7 +82,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'caco.sad',
+    'sad',
 )
 
 LOGIN_REDIRECT_URL = '/gda/'
